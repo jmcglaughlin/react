f40f5a772e06f4174884f0c12adb024f9dc166b3
John FT McGlaughlin <John.McGlaughlin@gmail.com>
2018-12-27 13:35:16 -0500

    [REACT] Alternative callback when only a single event is required

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -2,31 +2,21 @@ import React from 'react';
 
 class SearchBar extends React.Component {
   // callback function to allow feedback of input typing
-  onInputChange(event) {
-    // typicallly only interested in the value extracted this way
-    console.log(event.target.value);
-  }
+  // onInputChange(event) {
+  //   // typicallly only interested in the value extracted this way
+  //   console.log(event.target.value);
+  // }
 
-  // callback function to allow detection of input field click
-  onInputClick(even) {
-    console.log('input field was selected');
-  }
-
-  // () are left off of a callback function InputChange
-  // if () included call back function is called on every invocation of render.
-  // Leaving off () passes a reference of function to input element to use in
-  // future as in  onChange
+  // call back function is replace with an anonymous funciton
+  // performing single task as shorthand
+  // note: event is typically shortened to simply e
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
             <label>ImageSearch: </label>
-            <input
-              type="text"
-              onClick={this.onInputClick}
-              onChange={this.onInputChange}
-            />
+            <input type="text" onChange={e => console.log(e.target.value)} />
           </div>
         </form>
       </div>
