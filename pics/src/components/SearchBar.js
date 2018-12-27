62727a697a2d1a66053ee3b51d22d2885849d105
John FT McGlaughlin <John.McGlaughlin@gmail.com>
2018-12-27 14:52:49 -0500

    [REACT] change from an uncontrolled event handler to Contrlled event handler BRINGS data into React Compnonent not DOM

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,22 +1,19 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-  // callback function to allow feedback of input typing
-  // onInputChange(event) {
-  //   // typicallly only interested in the value extracted this way
-  //   console.log(event.target.value);
-  // }
+  state = { term: 'enter search term' };
 
-  // call back function is replace with an anonymous funciton
-  // performing single task as shorthand
-  // note: event is typically shortened to simply e
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
             <label>ImageSearch: </label>
-            <input type="text" onChange={e => console.log(e.target.value)} />
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={e => this.setState({ term: e.target.value })}
+            />
           </div>
         </form>
       </div>
