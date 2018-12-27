168505938ac41f646bdf28dcaaab3d55fa2e7e96
John FT McGlaughlin <John.McGlaughlin@gmail.com>
2018-12-27 16:10:54 -0500

    [REACT install axios for API features ise unspalsh for data

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
+
+const source = 'https://api.unsplash.com/search/photos',
+  authKey = '64102276ec4b87e450aeee53254979085bf04f67f84442fac9246f5f6fce7109';
 
 class App extends React.Component {
   // callback function for the child
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get(source, {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID ' + authKey
+      }
+    });
   }
 
   render() {
