d73763b8859746884cff74ff5597375763830300
John FT McGlaughlin <John.McGlaughlin@gmail.com>
2018-12-27 13:29:39 -0500

    [REACT] Add callback functions to input field

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,13 +1,32 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  // callback function to allow feedback of input typing
+  onInputChange(event) {
+    // typicallly only interested in the value extracted this way
+    console.log(event.target.value);
+  }
+
+  // callback function to allow detection of input field click
+  onInputClick(even) {
+    console.log('input field was selected');
+  }
+
+  // () are left off of a callback function InputChange
+  // if () included call back function is called on every invocation of render.
+  // Leaving off () passes a reference of function to input element to use in
+  // future as in  onChange
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
             <label>ImageSearch: </label>
-            <input type="text" />
+            <input
+              type="text"
+              onClick={this.onInputClick}
+              onChange={this.onInputChange}
+            />
           </div>
         </form>
       </div>
