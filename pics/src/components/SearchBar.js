133b159d79e39068a03410acc2c614da8790b70e
John FT McGlaughlin <John.McGlaughlin@gmail.com>
2018-12-27 15:20:54 -0500

    [REACT] fix 2 use anonymous function for form onSubmit note: onFromSubmit 'can' be => function (redundant)

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -4,17 +4,17 @@ class SearchBar extends React.Component {
   state = { term: 'enter search term' };
 
   // change to arrow function
-  onFormSubmit = event => {
+  onFormSubmit(event) {
     event.preventDefault();
 
     // ES5 binds this to arrow functions
     console.log(this.state.term);
-  };
+  }
 
   render() {
     return (
       <div className="ui segment">
-        <form onSubmit={this.onFormSubmit} className="ui form">
+        <form onSubmit={e => this.onFormSubmit(e)} className="ui form">
           <div className="field">
             <label>ImageSearch: </label>
             <input
