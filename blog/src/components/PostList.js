09b424c68aa1fb473797b3a356d95ebf23f1a814
John FT McGlaughlin <john.mcglaughlin@gmail.com>
2019-01-04 16:39:28 -0500

    [R-REDUX] Pre refactor UserHeader

--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
+import UserHeader from './UserHeader';
 
 class PostList extends React.Component {
   componentDidMount() {
     this.props.fetchPosts();
   }
 
+  renderList() {
+    return this.props.posts.map(post => {
+      return (
+        <div className="item" key={post.id}>
+          <i className="ui middle aligned icon user" />
+          <div className="content">
+            <div className="description">{post.title}</div>
+            <p>{post.body}</p>
+          </div>
+          <UserHeader userId={post.userId} />
+        </div>
+      );
+    });
+  }
+
   render() {
-    console.log(this.props.posts);
-    return <div>Post List</div>;
+    return <div className="ui relaxed divided list">{this.renderList()}</div>;
   }
 }
 
