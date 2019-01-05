c41435c37f4ae892adf1085d0faaa92195f515ed
John FT McGlaughlin <john.mcglaughlin@gmail.com>
2019-01-04 22:08:16 -0500

    [REACT] ALTERNATIVE solution to limit calls to /users

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -14,6 +14,20 @@ import jsonPlaceholder from '../apis/jsonPlaceholder';
 //   };
 // };
 
+// Now we can make use of getState to get access to Store
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  // make use of lodash specialty methods uniq and map
+  // _map pulls out the 'userId' parameter from posts array
+  // then _.uniq extracts only userIds
+  const userIds = _.uniq(_.map(getState().posts, 'userId'));
+
+  // we do not need an await because we don't care since nothing else is dependent on it
+  userIds.forEach(id => dispatch(fetchUser()));
+  // NOTE We are still calling 100 times!!!
+};
+
 // shortCode
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get('/posts');
@@ -22,6 +36,12 @@ export const fetchPosts = () => async dispatch => {
 };
 
 export const fetchUser = id => async dispatch => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({ type: 'FETCH_USER', payload: response.data });
+};
+
+export const fetchUser2 = id => dispatch => {
   _fetchUser(id, dispatch);
 };
 
